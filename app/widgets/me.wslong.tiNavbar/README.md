5885d8f274bf46c4a1e8839da88e0fac6b1279d6
Shallong <shallowlong@126.com>
2015-06-03 23:14:38 +0800

    [Add] Wrong structure adjustment: import the widget into the project

---
+++ app/widgets/me.wslong.tiNavbar/README.md
@@ -0,0 +1,121 @@
+tiNavbar
+--------
+
+A custom NavigationBar widget for Titanium Alloy, which provides the common navigation bar layout for cross-platform environments.
+
+This widget is based on [com.mcongrove.navigationBar](https://github.com/mcongrove/com.mcongrove.navigationBar).
+
+## Glance at first
+
+#### Alloy View (XML)
+
+```
+<Require type="widget" src="me.wslong.tiNavbar" id="navbar"/>
+```
+	
+#### Alloy Controller (JS)
+
+```
+$.navbar.setBackgroundColor("#35ABFF");
+
+$.navbar.showBack(
+	function(_event) {
+		closeWindow();
+	}
+);
+
+$.navbar.showNext({
+	function(_event) {
+		doSomething();
+	}
+});
+```
+
+## Notice
+
+**Recommanded TSS, XML contents**
+
+```
+app.tss
+		"Window":{
+		  navBarHidden: true,  // to disable the system default nav bar of IOS, for Android, need to use the customized theme
+		  height: "100%",
+		  width: "100%"
+		}
+		".innerContainerTop":{
+		  top: 47
+		}
+		".innerContainerTop[platform=ios]":{
+		  top: 67
+		}
+
+win.xml
+		<Alloy>
+		  <Window class="container">
+		    <Require type="widget" src="me.wslong.tiNavbar" id="navbar"/>
+		    <View class="innerContainerTop">
+		    	Your contents.
+		    </View>
+		  </Window>
+		</Alloy>
+```
+
+For Android, need to specify the **NO ActionBar theme** to be able to use this navigation bar only.
+
+
+#### API
+
+* init
+	- titleImage : set the title image if set, otherwise title text
+	- titleText : set the title text, empty by default
+	- titleColor : set the title color (Optional)
+	- fontFamily : set the title font family (Optional)
+	- backgroundColor : set the title background (Optional)
+	- borderColor : set the title border color, the same with title background by default
+	- theme : set the theme, "black" or "white", to be used when choosing widegt-provided icons
+* showLeft
+	- text : set the text if has, this is the first judgement
+	- image : set the image if has, this is the seconde judgement, otherwise using the default "back" icon
+	- callback
+* showRight
+	- text : set the text if has, this is the first judgement
+	- image : set the image if has, this is the seconde judgement, otherwise using the default "next" icon
+	- callback
+* showBack : use the default "back" icon
+	- callback
+* showNext : use the default "next" icon
+	- callback
+* showMenu : use the default "menu" icon
+	- callback
+* showSetting : use the default "setting" icon
+	- callback
+* showAction : use the default "action" icon
+	- callback
+
+
+## Plan
+
+1. Update README and support Android and Mobile Web demos and add screenshots/gifs to the demo
+2. Replace with vector icons to support different color
+
+
+## Changelog
+
+* 1.0 - init version
+
+
+## License
+
+Copyright 2015 Shallong
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ app/widgets/me.wslong.tiNavbar/controllers/widget.js
@@ -0,0 +1,300 @@
+/**
+ * me.wslong.tiNavbar
+ */
+var titleView,
+		titleImage ,
+		titleText,
+		titleColor,
+		fontFamily,
+		backgroundColor,
+		borderColor,
+		theme,
+		deviceVersion = parseInt(Titanium.Platform.version.split(".")[0], 10);
+
+/**
+ * Init the widget
+
+ * args.titleImage
+ * args.titleText
+ * args.titleColor
+ * args.fontFamily
+ * args.backgroundColor
+ * args.theme
+ */
+$.init = function(args) {
+	titleImage = args.titleImage || null;
+	titleText = args.titleText || '';
+	titleColor = args.titleColor || null;
+	fontFamily = args.fontFamily || null;
+	backgroundColor = args.backgroundColor || 'white';
+	borderColor = args.borderColor || backgroundColor;
+	theme = args.theme || '';
+
+	$.wrapper.backgroundColor = backgroundColor;
+	$.border.backgroundColor = borderColor;
+	if (!theme) {
+		// Checks the brightness of the background color, sets color of icons/text
+		if(hexToHsb(backgroundColor).b < 65) {
+			theme = "white";
+		} else {
+			theme = "black";
+		}
+	}
+
+	if(titleImage) {
+		titleView = Ti.UI.createImageView({
+			image: titleImage,
+			height: "26dp",
+			width: Ti.UI.SIZE,
+			top: "10dp",
+			bottom: "10dp",
+			preventDefaultImage: true
+		});
+	} else if (titleText) {
+		titleView = Ti.UI.createLabel({
+			top: "0dp",
+			left: "58dp",
+			right: "58dp",
+			height: "46dp",
+			font: {
+				fontSize: "18dp",
+				fontFamily: (fontFamily ? fontFamily : "HelveticaNeue-Medium")
+			},
+			color: (titleColor ? titleColor : (theme == "white" ? "#FFF" : "#000")),
+			textAlign: "center",
+			text: titleText
+		});
+	}
+
+	if(titleView) {
+		$.titleView.add(titleView);
+	}
+}
+
+/**
+ * Shows the left button
+ * @param {Object} _params
+ * @param {Function} _params.callback The function to run on left button press
+ * @param {String} _params.image The image to show for the left button
+ */
+$.showLeft = function(_params) {
+	if(_params && typeof _params.callback !== "undefined") {
+		var text = _params.text;
+		var image = _params.image;
+		if (text) {
+			$.left.add(createNavbarText(text));
+		} else if (image) {
+			$.left.add(createNavbarImage(image));
+		} else {
+			var defaultImage = (theme == "white" ? WPATH("/images/white/back.png") : WPATH("/images/black/back.png"));
+			$.left.add(createNavbarImage(defaultImage));
+		}
+		$.left.visible = true;
+		$.left.addEventListener("touchend", _params.callback);
+	}
+};
+
+/**
+ * Shows the right button
+ * @param {Object} _params
+ * @param {Function} _params.callback The function to run on right button press
+ * @param {String} _params.image The image to show for the right button
+ */
+$.showRight = function(_params) {
+	if(_params && typeof _params.callback !== "undefined") {
+		var text = _params.text;
+		var image = _params.image;
+		if (text) {
+			$.right.add(createNavbarText(text));
+		} else if (image) {
+			$.right.add(createNavbarImage(image));
+		} else {
+			var defaultImage = (theme == "white" ? WPATH("/images/white/next.png") : WPATH("/images/black/next.png"));
+			$.right.add(createNavbarImage(defaultImage));
+		}
+		$.right.visible = true;
+		$.right.addEventListener("touchend", _params.callback);
+	}
+};
+
+/**
+ * Shows the back button
+ * @param {Function} _callback The function to run on back button press
+ */
+$.showBack = function(_callback) {
+	if(_callback && typeof _callback !== "undefined") {
+		var image = (theme == "white" ? WPATH("/images/white/back.png") : WPATH("/images/black/back.png"));
+		$.left.add(createNavbarImage(image));
+		$.left.visible = true;
+		$.left.addEventListener("touchend", _callback);
+	}
+};
+
+/**
+ * Shows the next button
+ * @param {Function} _callback The function to run on next button press
+ */
+$.showNext = function(_callback) {
+	if(_callback && typeof _callback !== "undefined") {
+		var image = (theme == "white" ? WPATH("/images/white/next.png") : WPATH("/images/black/next.png"));
+		$.right.add(createNavbarImage(image));
+		$.right.visible = true;
+		$.right.addEventListener("touchend", _callback);
+	}
+};
+
+/**
+ * Shows the menu button
+ * @param {Function} _callback The function to run on action button press
+ */
+$.showMenu = function(_callback) {
+	if(_callback && typeof _callback !== "undefined") {
+		var image = (theme == "white" ? WPATH("/images/white/menu.png") : WPATH("/images/black/menu.png"));
+		$.left.add(createNavbarImage(image));
+		$.left.visible = true;
+		$.right.addEventListener("touchend", _callback);
+	}
+};
+
+/**
+ * Shows the settings button
+ * @param {Function} _callback The function to run on action button press
+ */
+$.showSettings = function(_callback) {
+	if(_callback && typeof _callback !== "undefined") {
+		var image = (theme == "white" ? WPATH("/images/white/settings.png") : WPATH("/images/black/settings.png"));
+		$.right.add(createNavbarImage(image));
+		$.right.visible = true;
+		$.right.addEventListener("touchend", _callback);
+	}
+};
+
+/**
+ * Shows the action button
+ * @param {Function} _callback The function to run on action button press
+ */
+$.showAction = function(_callback) {
+	if(_callback && typeof _callback !== "undefined") {
+		var image = (theme == "white" ? WPATH("/images/white/action.png") : WPATH("/images/black/action.png"));
+		$.right.add(createNavbarImage(image));
+		$.right.visible = true;
+		$.right.addEventListener("touchend", _callback);
+	}
+};
+
+function createNavbarImage(img) {
+	return Ti.UI.createImageView({
+		image: img,
+		height: "28dp",
+		width: Ti.UI.SIZE,
+		top: "9dp",
+		left: "9dp",
+		preventDefaultImage: true
+	});
+}
+
+function createNavbarText(txt) {
+	var lbl = Ti.UI.createLabel({
+		top: "9dp",
+		height: "28dp",
+		width: Ti.UI.SIZE,
+		font: {
+			fontSize: "17dp",
+			fontFamily: (fontFamily ? fontFamily : "HelveticaNeue-Medium")
+		},
+		// color: (titleColor ? titleColor : (theme == "white" ? "#FFF" : "#000")),
+		color: "#427EF2",
+		textAlign: "center",
+		text: txt
+	});
+	lbl.addEventListener('touchstart', function() {
+		lbl.opacity = 0.4;
+	});
+	lbl.addEventListener('touchend', function() {
+		lbl.opacity = 1.0;
+	});
+	return lbl;
+}
+
+/**
+ * Converts a hex color value to HSB
+ * @param {String} _hex The hex color to convert
+ */
+function hexToHsb(_hex) {
+	var result;
+
+	if(_hex.length < 6) {
+		result = /^#?([a-f\d]{1})([a-f\d]{1})([a-f\d]{1})$/i.exec(_hex);
+	} else {
+		result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(_hex);
+	}
+
+	var hsb = {
+		h: 0,
+		s: 0,
+		b: 0
+	};
+
+	if(!result) {
+		return hsb;
+	}
+
+	if(result[1].length == 1) {
+		result[1] = result[1] + result[1];
+		result[2] = result[2] + result[2];
+		result[3] = result[3] + result[3];
+	}
+
+	var rgb = {
+		r: parseInt(result[1], 16),
+		g: parseInt(result[2], 16),
+		b: parseInt(result[3], 16)
+	};
+
+	rgb.r /= 255;
+	rgb.g /= 255;
+	rgb.b /= 255;
+
+	var minVal = Math.min(rgb.r, rgb.g, rgb.b),
+		maxVal = Math.max(rgb.r, rgb.g, rgb.b),
+		delta = maxVal - minVal,
+		del_r, del_g, del_b;
+
+	hsb.b = maxVal;
+
+	if(delta !== 0) {
+		hsb.s = delta / maxVal;
+
+		del_r = (((maxVal - rgb.r) / 6) + (delta / 2)) / delta;
+		del_g = (((maxVal - rgb.g) / 6) + (delta / 2)) / delta;
+		del_b = (((maxVal - rgb.b) / 6) + (delta / 2)) / delta;
+
+		if(rgb.r === maxVal) {
+			hsb.h = del_b - del_g;
+		} else if(rgb.g === maxVal) {
+			hsb.h = (1 / 3) + del_r - del_b;
+		} else if(rgb.b === maxVal) {
+			hsb.h = (2 / 3) + del_g - del_r;
+		}
+
+		if(hsb.h < 0) {
+			hsb.h += 1;
+		}
+
+		if(hsb.h > 1) {
+			hsb.h -= 1;
+		}
+	}
+
+	hsb.h = Math.round(hsb.h * 360);
+	hsb.s = Math.round(hsb.s * 100);
+	hsb.b = Math.round(hsb.b * 100);
+
+	return hsb;
+}
+
+// Move the UI down if iOS7+
+if(OS_IOS && deviceVersion >= 7) {
+	$.wrapper.height = "67dp";
+	$.overlay.top = "20dp";
+}
